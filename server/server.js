e459b67b0c71d16aa28694a60601d4053a8a0850
Eliza Meeks <elizameeks@gmail.com>
2017-12-27 15:24:56 -0600

    Setting up express some more

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const twit = require('twit');
 const dotenv = require('dotenv').load();
-
+const firebase = require('firebase');
+const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../client')));
+// app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+// app.use(bodyParser.json());                                     // parse application/json
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+// app.use(methodOverride());
+
+console.log(path.join(__dirname, '../client'));
 let twitterCredentials = {
     consumer_key: process.env.twconsumer_key,
     consumer_secret: process.env.twconsumer_secret,
@@ -11,13 +21,14 @@ let twitterCredentials = {
     timeout_ms: process.env.twtimeout_ms
 };
 
-var twitter = new twit(twitterCredentials);
+let twitter = new twit(twitterCredentials);
 
-var options = { screen_name: "Smingleigh", count: 1000, include_rts: true, exclude_replies: false, tweet_mode: 'extended', isStreaming: true };
-var thing = twitter.get('statuses/home_timeline', options, (err, tweeties) => {return tweeties});
+let options = { screen_name: "Smingleigh", count: 1000, include_rts: true, exclude_replies: false, tweet_mode: 'extended', isStreaming: true };
+let thing = twitter.get('statuses/home_timeline', options, (err, tweeties) => {return tweeties});
 app.listen(process.env.PORT || 5000);
-console.log('listening! 5000');
+console.log('listening!', process.env.PORT);
 
 app.get('/', (req, res) => {
     res.send(thing);
+    console.log("something has been got");
 });
