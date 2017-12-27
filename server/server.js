cb33e9807302770a7fb566acc17d9a5d74610763
Smingleigh <smingleigh@gmail.com>
2017-12-27 14:50:30 -0600

    The server now serves! It serves a raw heaping spoon of json into the client's face.
    DELICIOUS.

--- server/server.js
+++ server/server.js
@@ -11,14 +11,13 @@ let twitterCredentials = {
     timeout_ms: process.env.timeout_ms
 };
 
-var T = new twit(twitterCredentials);
+var twitter = new twit(twitterCredentials);
 
 var options = { screen_name: "Smingleigh", count: 1000, include_rts: true, exclude_replies: false, tweet_mode: 'extended', isStreaming: true };
+var thing = twitter.get('statuses/home_timeline', options, (err, tweeties) => {return tweeties});
+app.listen(process.env.PORT || 5000);
+console.log('listening! 5000');
 
-T.get('statuses/home_timeline', options, (err, data) => {
-    console.log("data length", data.length)
-    for (let i = 0; i< data.length; i++){
-        console.log(data[i].user.screen_name + ':');
-        console.log(data[i].full_text);
-    }
+app.get('/', (req, res) => {
+    res.send(thing);
 });
