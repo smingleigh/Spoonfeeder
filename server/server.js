e0b1c60adc3c4f663bd48dd3b89f03934a440824
Smingleigh <smingleigh@gmail.com>
2017-12-28 08:45:09 -0600

    Starting actual API stuff.

--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ const twit = require('twit');
 const firebase = require('firebase');
 const bodyParser = require('body-parser');
 const app = express();
-const path = require('path'); 
+const path = require('path');
+const routes = require('./api/routes/routes');
+routes(app);
 
 app.use(express.static(path.join(__dirname, '../client')));
 
@@ -16,23 +18,10 @@ console.log('Starting Spoonful server...');
 // app.use(methodOverride());
 
 console.log('Serving client from', path.join(__dirname, '../client'));
-
-let twitterCredentials = {
-    consumer_key: process.env.twconsumer_key,
-    consumer_secret: process.env.twconsumer_secret,
-    access_token: process.env.twaccess_token,
-    access_token_secret: process.env.twaccess_token_secret,
-    timeout_ms: process.env.twtimeout_ms
-};
-
-let twitter = new twit(twitterCredentials);
-
-let options = { screen_name: "Smingleigh", count: 1000, include_rts: true, exclude_replies: false, tweet_mode: 'extended', isStreaming: true };
-let thing = twitter.get('statuses/home_timeline', options, (err, tweeties) => {return tweeties});
 app.listen(process.env.PORT || 5000);
 console.log('Spoonful started on http://localhost:' + process.env.PORT);
 
 app.get('/', (req, res) => {
     res.send(thing);
-    console.log("Delivered 1 heaping spoon to", req.ip);
+    console.log("Delivering client to", req.ip);
 });
