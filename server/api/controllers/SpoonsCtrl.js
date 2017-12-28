943eb0cdda9917e469b48d1120cb9db67ad076b5
Smingleigh <smingleigh@gmail.com>
2017-12-28 11:00:09 -0600

    Added /user, still doing firebase stuff.

--- server/api/controllers/SpoonsCtrl.js
+++ server/api/controllers/SpoonsCtrl.js
@@ -1,16 +1,28 @@
 const firebase = require('firebase');
+require("firebase/firestore");
 
-var config = {
+firebase.initializeApp({
     apiKey: process.env.fbapiKey,
     authDomain: process.env.fbauthDomain,
-    databaseURL: process.env.fbdatabaseUrl,
-    storageBucket: process.env.fbprojectId,
-    messagingSenderId: process.env.fbmessagingSenderId,
-  };
-  firebase.initializeApp(config);
+    projectId: process.env.fbprojectId
+});
 
-exports.update = function(req, res) {
-    console.log(req.method, req.path, 'serving up a heaping spoon of test details to', req.hostname, req.ip);
-};
+let db = firebase.firestore();
+
+exports.dump = function(req, res) {
+    console.log(req.method, req.path, 'serving up a heaping spoon to', req.hostname, req.ip);
+
+    res.send("DUMP IT ALL! NOW!");
+}
+
+let modelData = {
+    user: 12,
+    data: {
+        1:5,
+        2:3,
+        5:1,
+        777:2
+    }
+}
 
 console.log('Set up SpoonsCtrl');
