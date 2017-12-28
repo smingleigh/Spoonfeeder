3f67f803961247b5d6109fca00be949cdbba99fb
Smingleigh <smingleigh@gmail.com>
2017-12-28 08:21:15 -0600

    More setting up. Making sure dotenv works. Making sure .env files aren't checked in. Generally being dumb.

--- server/server.js
+++ server/server.js
@@ -1,18 +1,22 @@
+console.log('Initializing...');
+const dotenv = require('dotenv').load();
 const express = require('express');
 const twit = require('twit');
-const dotenv = require('dotenv').load();
 const firebase = require('firebase');
 const bodyParser = require('body-parser');
 const app = express();
-const path = require('path');
+const path = require('path'); 
 
 app.use(express.static(path.join(__dirname, '../client')));
+
+console.log('Starting Spoonful server...');
 // app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 // app.use(bodyParser.json());                                     // parse application/json
 // app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 // app.use(methodOverride());
 
-console.log(path.join(__dirname, '../client'));
+console.log('Serving client from', path.join(__dirname, '../client'));
+
 let twitterCredentials = {
     consumer_key: process.env.twconsumer_key,
     consumer_secret: process.env.twconsumer_secret,
@@ -26,9 +30,9 @@ let twitter = new twit(twitterCredentials);
 let options = { screen_name: "Smingleigh", count: 1000, include_rts: true, exclude_replies: false, tweet_mode: 'extended', isStreaming: true };
 let thing = twitter.get('statuses/home_timeline', options, (err, tweeties) => {return tweeties});
 app.listen(process.env.PORT || 5000);
-console.log('listening!', process.env.PORT);
+console.log('Spoonful started on http://localhost:' + process.env.PORT);
 
 app.get('/', (req, res) => {
     res.send(thing);
-    console.log("something has been got");
+    console.log("Delivered 1 heaping spoon to", req.ip);
 });
